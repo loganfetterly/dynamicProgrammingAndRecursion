2c6499c4a2d458f5b17988010d831b9b016d0ba9
Logan Fetterly <lfetterly25@gmail.com>
2020-06-26 16:47:43 -0600

    Added some extra prints

--- final-fib.js
+++ final-fib.js
@@ -3,6 +3,7 @@
 // Here is the implementation with time complexity of 2^n
 // Parameters: n is index in the series we wish to find
 function fib(n) {
+    funcCallsFib += 1;
     // Use a switch statement to return the first 3 indexes else do recursion as the default
     switch (n) {
         case 0:
@@ -20,6 +21,7 @@ function fib(n) {
 // Here is the implementation with linear time
 // Parameters: n is index in the series we wish to find
 function dynamicFib(n) {
+    funcCallsDynamicFib += 1;
     if (cache[n]) {
         return cache[n];
     } else {
@@ -43,11 +45,17 @@ function dynamicFib(n) {
 
 var cache = [];
 var comp = 40;
+var funcCallsFib = 0;
+var funcCallsDynamicFib = 0;
 
-console.log("We are going to find the Fib unmbers for: " + comp);
+console.log("We are going to find the Fib numbers for: " + comp);
+console.log("Solving with time complexity O(2^n)..");
+
 console.time('fib')
-console.log(fib(comp));
+console.log(fib(comp) + " found with " + funcCallsFib + " function calls.");
 console.timeEnd('fib')
+
+console.log("Solving with time complexity O(n)..");
 console.time('dynamicFib')
-console.log(dynamicFib(comp));
+console.log(dynamicFib(comp) + " found with " + funcCallsDynamicFib + " function calls.");
 console.timeEnd('dynamicFib')
